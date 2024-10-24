cde06c4d0a9fe0937f37a5e18771f9e043f8f017
upayanmazumder <upayanm3@gmail.com>
2024-10-24 18:44:06 +0530

    reverted that stupid commit

--- qwik/src/lib/firestore.ts
+++ qwik/src/lib/firestore.ts
@@ -1,6 +1,10 @@
 import { initFirestore } from "@auth/firebase-adapter";
-import { applicationDefault } from "firebase-admin/app";
+import { cert } from "firebase-admin/app";
 
 export const firestore = initFirestore({
-  credential: applicationDefault(),
+  credential: cert({
+    projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+    clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
+  }),
 });
