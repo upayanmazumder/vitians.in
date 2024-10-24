ca54293f6ee97e20ee6f853b70e634dea1171562
upayanmazumder <upayanm3@gmail.com>
2024-10-24 18:06:14 +0530

    Refactor authentication provider and add Firestore adapter

--- qwik/src/routes/plugin@auth.ts
+++ qwik/src/routes/plugin@auth.ts
@@ -1,19 +1,20 @@
-import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { QwikAuth$ } from "@auth/qwik";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { firestore } from "../lib/firestore";
 import Google from "@auth/qwik/providers/google";
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
     providers: [
       Google({
-        authorization: {
-          params: {
-            hd: 'vitstudent.ac.in', // restrict to VIT students
-          },
-        },
+        // authorization: {
+        //   params: {
+        //     hd: 'vitstudent.ac.in', // restrict to VIT students
+        //   },
+        // },
       }),
     ],
-    adapter: FirestoreAdapter(),
+    adapter: FirestoreAdapter(firestore),
     pages: {
       signIn: "/a/signin/",
       signOut: "/a/signout/",
